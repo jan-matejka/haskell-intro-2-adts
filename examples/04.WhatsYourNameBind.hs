645d25d733e6bb5ca9c8426a4d1d33467a4cd2f0
Jan (yac) Matějka <JMatejka@suse.cz>
2014-10-03 23:17:42 +0200

    Show GHCi and explain type signatures
    Better explain the bind

--- examples/04.WhatsYourNameBind.hs
+++ examples/04.WhatsYourNameBind.hs
@@ -3,8 +3,3 @@ welcome name      = putStrLn ("Hello " ++ name)
 _getLine  unused  = getLine
 
 main = ask >>= _getLine >>= welcome
-
-
--- (>>=) :: Monad m => m a -> (a -> m b) -> m b
--- lhs  >>=  rhs
--- execute lhs and pass it's result into rhs
